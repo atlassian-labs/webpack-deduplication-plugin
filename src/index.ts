8ace13411420ab8268e0bb091150567a9712a5c8
Nadia Makarevich <nmakarevich@atlassian.com>
2020-03-23 14:55:18 +1100

    basic typescript and tests setup

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sum = (a: number, b: number): number => {
+    return a + b;
+}
+
+const t = sum(2, 1);
+
+module.exports = sum;
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+const s = require('./index.ts');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(s(1, 2)).toBe(3);
+});
